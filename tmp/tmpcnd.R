fc22ef396dd3b9a4c00e9304c81d7411adc93a16
cora1ine <gezhichen1031@gmail.com>
2024-03-28 00:50:19 +1100

    upload data (CPI, GDP, Cash rate target)

--- tmp/tmpcnd.R
+++ tmp/tmpcnd.R
@@ -1,2 +1,30 @@
-library(vars)
-data(Canada)
+##### RP1
+
+# install.packages("readabs")
+# install.packages("readrba")
+library(readabs)
+library(readrba)
+library(xts)
+
+### Data downloading
+
+# Inflation / CPI 
+# 6401.0 Consumer Price Index, Australia
+# series_id = "A2325846C": Index Numbers ;  All groups CPI ;  Australia ;
+cpi_download = read_abs(series_id = "A2325846C")     
+cpi_data     = xts(cpi_download$value, cpi_download$date)
+
+# GDP
+# 5206.0 Australian National Accounts: National Income, Expenditure and Product
+# series_id = "A2304370T": Gross domestic product: Chain volume measures - Percentage changes
+gdp_download = read_abs(series_id = "A2304370T")     
+gdp_data     = xts(cpi_download$value, cpi_download$date)
+
+# Cash rate target
+
+crt_download = read_rba(series_id = "FIRMMCRTD")   
+crt_data     = xts(crt_download$value, crt_download$date)
+crt_data     = apply.quarterly(crt_data,mean)     # change daily to quarterly
+
+
+
